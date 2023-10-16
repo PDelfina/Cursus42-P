2f30db4a8c8a9a35dac672965abf68758934f7f3
PDelfina <delfiparada05@gmail.com>
2023-10-16 10:44:36 +0200

    LIBFT finished and moulinette passed. Without mains.

---
+++ LIBFT/ft_lstadd_back_bonus.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dparada <dparada@student.42malaga.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/05 16:39:53 by dparada           #+#    #+#             */
+/*   Updated: 2023/10/11 10:29:50 by dparada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list	*aux;
+
+	if (*lst == NULL)
+		*lst = new;
+	else
+	{
+		aux = ft_lstlast(*lst);
+		aux->next = new;
+	}
+}
---
+++ LIBFT/ft_strjoin.c
@@ -0,0 +1,41 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dparada <dparada@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/28 13:39:13 by dparada           #+#    #+#             */
+/*   Updated: 2023/10/11 10:38:21 by dparada          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	char	*ptr;
+	int		i;
+	int		j;
+
+	i = ft_strlen(s1);
+	j = ft_strlen(s2);
+	ptr = (char *) malloc((i + j + 1) * sizeof(char));
+	if (ptr == NULL)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i])
+	{
+		ptr[i] = s1[i];
+		i++;
+	}
+	while (s2[j])
+	{
+		ptr[i] = s2[j];
+		i++;
+		j++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
